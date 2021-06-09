5ad0b797ad1d84bcf5c5e8d89184004835bc0e1a
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2021-06-09 18:56:18 -0300

    reserved words

---
+++ reservadas.rb
@@ -0,0 +1,35 @@
+
+#palavras reservadas Ruby
+s = <<FIM
+alias 
+and 
+BEGIN 
+begin 
+break 
+case 
+class 
+def 
+de
+ned 
+do 
+else 
+elsif 
+END FIM
+not 
+or 
+redo 
+rescue 
+retry 
+return 
+self 
+super 
+then 
+true 
+undef 
+unless 
+until
+when 
+while
+yield
+FIM
+puts s
