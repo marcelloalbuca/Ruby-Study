68b3b23744b9c77a877f242303c0cddf1f2e4aab
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2021-06-09 00:15:23 -0300

    String

---
+++ string.rb
@@ -0,0 +1,32 @@
+nome = "Marcello"
+puts nome
+
+mensagem = "Bem vindo #{nome}"
+puts mensagem
+
+mensagem2 = "Bem vindo #{1 + 3}"
+puts mensagem2
+
+mensagem3 = "Bem vindo " + nome
+puts mensagem3
+
+mensagem4 = <<~MENSAGEM
+OLÁ RUBY CURSO
+MENSAGEM
+puts mensagem4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
