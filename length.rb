3b571c1bd36b654e28b8e890f313e23c3948c62a
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2021-06-09 19:09:05 -0300

    method length

---
+++ length.rb
@@ -0,0 +1,14 @@
+#TODOS OS METHODOS STRING
+##puts mensagem6.public_methods
+
+nome = "Marcello"
+mensagem6 = %Q(teste string #{nome})
+puts mensagem6
+
+
+puts mensagem6.length
+
+print "olá mundo \n"
+p "Olá mundo 2"
+
+puts "oi comentário"
