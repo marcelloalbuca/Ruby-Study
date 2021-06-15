85af77097907ec897831a449bd00ba85f96cd4b7
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2021-06-15 00:19:51 -0300

    comment block, interpolaçao

--- hello_world.rb
+++ hello_world.rb
@@ -1,5 +1,12 @@
 #COMENTÁRIO
 
+#COMENTÁRIO EM BLOCO
+
+=begin
+mensagem teste
+=end
+
+
 puts "Hello World"
 
 nome = "Marcello"
@@ -7,6 +14,14 @@ nome = "Marcello"
 #Interpolação
 mensagem = "BeM vindo #{nome}"
 puts mensagem
+
+
+#Não faz concatenação(interpolação)
+mensagem5 = %q(teste string)
+puts mensagem5
+
+mensagem6 = %Q(teste string #{nome})
+puts mensagem6
 
 #String
 nome = "Cadu"
