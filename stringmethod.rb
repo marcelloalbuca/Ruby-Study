2ad0c9737ea956d2e2546741a5f757205de53d50
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2021-06-15 00:29:55 -0300

    .strip, .chars, substring

---
+++ stringmethod.rb
@@ -0,0 +1,15 @@
+#pegar um caracter da String
+puts "marcello"[0]
+
+#substring
+puts "marcello"[3,7]
+
+#todos caracteres como arrays
+puts "cadu".chars
+
+#multiplicação de String
+puts "M" * 10
+
+#remover espaços
+puts "  teste 1  2 444   ".strip
+
